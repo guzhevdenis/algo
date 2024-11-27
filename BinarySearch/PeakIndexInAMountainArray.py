4d467b41179ccd549952ef4f8a3f5e69f95baf9f
Denis Guzhev <guzhev.98@mail.ru>
2024-11-27 10:38:47 +0300

    Added Peak Index in a mountain array

---
+++ BinarySearch/PeakIndexInAMountainArray.py
@@ -0,0 +1,25 @@
+# You are given an integer mountain array arr of length 
+# n where the values increase to a peak element 
+# and then decrease.
+
+# Return the index of the peak element.
+
+# Your task is to solve it in O(log(n)) time complexity.
+def peakIndexInMountainArray(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while (left <= right):
+        middle = left +(right-left)//2
+        if (arr[middle-1] < arr[middle] > arr[middle+1]):
+            return middle
+        elif (arr[middle-1] < arr[middle+1] > arr[middle]):
+            left = middle + 1
+        elif (arr[middle+1] < arr[middle-1] > arr[middle]):
+            right = middle -1
+
+
+
+
+nums = [0,1,5,3,6,1,2,7]
+print(peakIndexInMountainArray(nums))
