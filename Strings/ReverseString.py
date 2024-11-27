e2e87218fecad51f7049de28f6999fa21a0edd71
Denis Guzhev <guzhev.98@mail.ru>
2024-11-27 14:35:02 +0300

    Add Reverse String

---
+++ Strings/ReverseString.py
@@ -0,0 +1,10 @@
+# Write a function that reverses a string. 
+# The input string is given as an array of characters s.
+
+# You must do this by modifying the input array
+# in-place with O(1) extra memory.
+
+def reverseString(s):
+    for i in range (len(s) //2):
+        swapping_value = s[i]
+        s[i] = s[len(s)-1-i] 
