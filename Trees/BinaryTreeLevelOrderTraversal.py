1bbe9cf7a1282d1dda800cc3046b0087bd640a1f
Denis Guzhev <guzhev.98@mail.ru>
2024-12-03 14:17:31 +0300

    Added BinaryTreeLevelOrderTraversal

---
+++ Trees/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,48 @@
+#Level Order Traversal - BDS
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def levelorderTraversal (root):
+    if root is None:
+        return []
+    
+    answer = []
+    level_nodes = []
+    cur_nodes = []
+
+    level_nodes.append(root)
+    
+    
+    while (level_nodes):
+
+        answer.append([element.val for element in level_nodes])
+        cur_nodes = level_nodes
+        level_nodes = []
+
+        while (cur_nodes):
+
+            node = cur_nodes[0]
+            cur_nodes.pop(0)
+
+            if(node.left):
+                level_nodes.append(node.left)
+            if(node.right):
+                level_nodes.append(node.right) 
+        
+    return answer
+    
+    
+
+    
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print(levelorderTraversal(root))
